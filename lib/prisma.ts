d3939e8d5ae649de88ca0bb667bca66789663118
muslewski <mateusz.mus@proton.me>
2025-02-14 12:01:18 +0100

    Change from prisma neon to prisma accelerate

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,25 +1,44 @@
-import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
-import { Pool, neonConfig } from "@neondatabase/serverless";
+// import { PrismaClient } from "@prisma/client";
+// import { PrismaNeon } from "@prisma/adapter-neon";
+// import { Pool, neonConfig } from "@neondatabase/serverless";
 
 // import ws from "ws";
 // neonConfig.webSocketConstructor = ws;
 
-// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
-neonConfig.poolQueryViaFetch = true;
-// Type definitions
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
+// // To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
+// neonConfig.poolQueryViaFetch = true;
+// // Type definitions
+// declare global {
+//   // eslint-disable-next-line no-var
+//   var prisma: PrismaClient | undefined;
+// }
 
-const connectionString = `${process.env.DATABASE_URL}`;
+// const connectionString = `${process.env.DATABASE_URL}`;
 
-const pool = new Pool({ connectionString });
-const adapter = new PrismaNeon(pool);
+// const pool = new Pool({ connectionString });
+// const adapter = new PrismaNeon(pool);
 
-const db = globalThis.prisma || new PrismaClient({ adapter });
+// const db = globalThis.prisma || new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV !== "production") global.prisma = db;
+// if (process.env.NODE_ENV !== "production") global.prisma = db;
+
+// export default db;
+
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+
+// Learn more about instantiating PrismaClient in Next.js here: https://www.prisma.io/docs/data-platform/accelerate/getting-started
+
+const prismaClientSingleton = () => {
+  return new PrismaClient().$extends(withAccelerate());
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const db = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export default db;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = db;
